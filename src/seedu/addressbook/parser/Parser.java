962714a3867b3206316dbaa0cd33a79abe90b7e2
crumpledpaper <ong.junhao.bryan@dhs.sg>
2019-02-12 22:26:02 +0800

    added parser test for update command and fixed bug in parser

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -43,7 +43,7 @@ public class Parser {
                     + "(?<tagArguments>(?: t/[^/]+)*)"); // variable number of tags
 
     public static final Pattern PERSON_INDEX_AND_DATA_ARGS_FORMAT =
-            Pattern.compile("(?<targetIndex>.+)"
+            Pattern.compile("(?<targetIndex>[\\d]+)"
                     + " (?<name>[^/]+)"
                     + " (?<isPhonePrivate>p?)p/(?<phone>[^/]+)"
                     + " (?<isEmailPrivate>p?)e/(?<email>[^/]+)"
